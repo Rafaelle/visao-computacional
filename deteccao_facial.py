054ff00ee9699af88a1d7b214a06724ee990338e
Rafaelle Amorim <Rafaelle@users.noreply.github.com>
2018-07-22 20:32:46 -0300

    Adicionando captura de imagem

--- deteccao_facial.py
+++ deteccao_facial.py
@@ -2,6 +2,11 @@ import cv2
 
 classificador = cv2.CascadeClassifier("haarcascade_frontalface_default.xml") # carregar arquivo
 camera = cv2.VideoCapture(0) # capturar imagem da webcam 
+amostra = 1 # controlar quantas fotos são tiradas na web (video)
+numeroAmostras = 10 # valor ainda será estudado
+# adicionar identificador para pessoas diferentes
+
+print("Capturando...")
 
 while (True) :
     conectcado, imagem = camera.read() # conectar e ler imagem da camera
@@ -25,13 +30,17 @@ while (True) :
         cv2.rectangle(imagem, (x, y), (x + l, y + a), (0, 0, 255), 5) 
         # rectangle(imagem, inicio, final, rgb, borda)
 
-    cv2.imshow("Face" , imagem) # mostrar imagem com face detectada
+        # quando a tecla Q for pressionada, capturar imagem
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            cv2.imwrite("fotos/face_" + str(amostra) + ".jpg", cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY))
+            print("Foto " + str(amostra) + " capturada")
+            amostra += 1
 
-    #parar o código e fechar a janela
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    cv2.imshow("Face", imagem)
+    # quando chegar no número de amostras para o programa
+    if (amostra >= numeroAmostras + 1) : 
+        break     
 
-
-
+print ("Finalizado!")
 camera.release() # liberar a memoria
 cv2.destroyAllWindows() # fechar todas as janelas
